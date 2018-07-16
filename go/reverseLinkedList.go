c1e8b36c74214e6eca37b834516516b5cd33f0de
Krunal Naik <naik1985@users.noreply.github.com>
2018-07-15 18:59:37 -0700

    Create reverseLinkedList.go

---
+++ go/reverseLinkedList.go
@@ -0,0 +1,12 @@
+func reverseList(head *ListNode) *ListNode {
+    var reverseListNode *ListNode
+    reverseListNode = nil
+    for head != nil {
+        var tempListNode *ListNode
+        tempListNode = head.Next
+        head.Next = reverseListNode
+        reverseListNode = head
+        head = tempListNode
+    }
+    return reverseListNode 
+}
